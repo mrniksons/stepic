5f27514f98691fbfd587b8b828bf0442eaa491d4
Nick <87988680+mrniksons@users.noreply.github.com>
2023-01-29 17:56:14 +0100

    Test
    First Test Commit

---
+++ 2-4-5.py
@@ -0,0 +1,42 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import time
+import math
+
+browser = webdriver.Chrome()
+
+browser.get("http://suninjuly.github.io/explicit_wait2.html")
+
+#formula
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+try:
+    #смотрим счетчик цены до 100
+    price = WebDriverWait(browser, 13).until(EC.text_to_be_present_in_element((By.ID, 'price'), '100'))
+    button = browser.find_element(By.CSS_SELECTOR, "#book")
+    button.click()
+
+    #решаем капчу
+    #calculate_y
+    x_element = browser.find_element(By.ID, "input_value")
+    x = x_element.text
+    y = calc(x)
+    print(y)    #выводим результат формулы чтобы проверить
+
+
+    #enter_answer
+    input1 = browser.find_element(By.ID, 'answer')
+    input1.send_keys(y)
+
+    #submit
+    submit = browser.find_element(By.XPATH, "/html/body/form/div/div/button")
+    submit.click()
+
+
+finally:
+    time.sleep(5)
+    browser.quit()
